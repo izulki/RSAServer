3416fa960646b951e0f776965cd5849e7c84138d
Irfan <maulana@dal.ca>
2019-03-29 15:11:50 +0000

    Added route for message list response

--- server.js
+++ server.js
@@ -46,12 +46,62 @@ app.get('/send/:message/:time/:username', function(req, res) {
         if (!fs.existsSync(MSG_DIR)){
             fs.mkdirSync(MSG_DIR);
         } else {
-            fs.writeFile(MSG_DIR+req.params.time+'.txt', req.params.message, function(err) {
+            fs.writeFile(MSG_DIR+req.params.time+'.json', req.params.message, function(err) {
                 if(err) {
                     return console.log(err);
                 }
             }); 
             res.send('SEND OK');
+        }
+    }
+});
+
+
+/********** GET MESSAGE LIST **********/
+app.get('/recv/list/:username', function(req, res) {
+    REG_DIR = __dirname+'/users/'+req.params.username+'/'
+    MSG_DIR = REG_DIR + 'messages/' 
+    if (!fs.existsSync(REG_DIR)){
+        res.send('USER NOT FOUND');
+        return console.log("USER NOT FOUND");
+    } else {
+        if (!fs.existsSync(MSG_DIR)){
+            fs.mkdirSync(MSG_DIR);
+        } else {
+            var list = {};
+            fs.readdir(MSG_DIR, function (err, files) {
+                //handling error
+                if (err) {
+                    return console.log('Unable to scan directory: ' + err);
+                } 
+                //listing all files using forEach
+                // files.forEach(function (file) {
+                // });
+                res.json(files);
+                console.log(files);
+            });
+            
+        }
+    }
+});
+
+/********** GET MESSAGE **********/
+app.get('/recv/:time/:username', function(req, res) {
+    REG_DIR = __dirname+'/users/'+req.params.username+'/'
+    MSG_DIR = REG_DIR + 'messages/' 
+    if (!fs.existsSync(REG_DIR)){
+        res.send('USER NOT FOUND');
+        return console.log("USER NOT FOUND");
+    } else {
+        if (!fs.existsSync(MSG_DIR)){
+            fs.mkdirSync(MSG_DIR);
+        } else {
+            if (fs.existsSync(MSG_DIR+req.params.time+'.json')) {
+                res.download(MSG_DIR+req.params.time+'.json')
+            } else {
+                res.send('FILE NOT FOUND');
+            }
+
         }
     }
 });
